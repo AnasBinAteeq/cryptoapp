1e223a7daaf26ab31a3553358d5eeafa0da3c6c7
anasbinateeq <anasbinateeq15@gmail.com>
2024-02-23 19:42:04 +0500

    All comp implemented

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Row, Col, Typography } from "antd";
-import { Line } from "react-chartjs-2";
+
+import React from 'react';
+import Chart from 'chart.js/auto'
+import { Line } from 'react-chartjs-2';
+import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
@@ -8,47 +10,51 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
 
-  // Push coin price and timestamp data into arrays
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
-    coinTimestamp.push(
-      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
-    );
   }
 
-  // Define chart data
-  // const data = {
-  //   labels: coinTimestamp,
-  //   datasets: [
-  //     {
-  //       label: "Price In USD",
-  //       data: coinPrice,
-  //       fill: false,
-  //       backgroundColor: "#0071bd",
-  //       borderColor: "#0071bd",
-  //     },
-  //   ],
-  // };
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    console.log(coinHistory?.data?.history[i].timestamp);
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp * 1000).toLocaleDateString());
+  }
+  const data = {
+    labels: coinTimestamp.reverse(),
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice.reverse(),
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
 
-  // Define chart options
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  console.log(coinTimestamp)
 
   return (
     <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">
-          {coinName} Price Chart{" "}
-        </Title>
+        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">
-            Change: {coinHistory?.data?.change}%
-          </Title>
-          <Title level={5} className="current-price">
-            Current {coinName} Price: $ {currentPrice}
-          </Title>
+          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
+          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
-      {/* Render Line chart */}
-      {/* <Line data={data} /> */}
+      <Line data={data} options={options} />
     </>
   );
 };
